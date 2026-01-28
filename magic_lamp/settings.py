50b7754c9f350bc25b3091bafbfb507787519dfa
Pramod Gopinath <pramodgopinath@Pramods-MacBook-Air.local>
2026-01-28 12:35:00 +0530

    cores

--- magic_lamp/settings.py
+++ magic_lamp/settings.py
@@ -71,12 +71,39 @@ SWAGGER_SETTINGS = {
     },
 }
 
-CORS_ALLOWED_ORIGINS = [
+# CORS_ALLOWED_ORIGINS = [
    
-    "http://localhost:3000",
-    "http://localhost:5174",
-    "http://127.0.0.1:8000",
+#     "http://localhost:3000",
+#     "http://localhost:5174",
+#     "http://127.0.0.1:8000",
 
+# ]
+
+# Allow all origins
+CORS_ALLOW_ALL_ORIGINS = True
+
+# Optional: Additional CORS settings
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOWED_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 # Configure session settings
